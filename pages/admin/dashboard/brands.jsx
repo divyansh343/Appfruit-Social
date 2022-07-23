11570b202e976b6d7cde45c070f2301de46e1a82
divyansh pal <70535666+divyansh343@users.noreply.github.com>
2022-07-23 15:28:42 +0530

    minimal change

--- pages/admin/dashboard/brands.jsx
+++ pages/admin/dashboard/brands.jsx
@@ -54,7 +54,7 @@ const Page = () => {
             <thead>
               <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
                 <th className="px-4 py-3">Name/Brand name</th>
-                <th className="px-4 py-3">Desciption</th>
+                <th className="px-4 py-3">Description</th>
                 <th className="px-4 py-3">Phone</th>
                 <th className="px-4 py-3">Email</th>
                 <th className="px-4 py-3">Budget</th>
