c40570b47406fd13edbd3330f19d1d5c8502f8c5
divyansh pal <70535666+divyansh343@users.noreply.github.com>
2022-07-27 10:05:05 +0530

    Integrated google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,19 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet" />
+
+                    <script strategy="lazyOnload" async src={`https://www.googletagmanager.com/gtag/js?id=G-ZX1S3FS4P4`}></script>
+                    <script strategy="lazyOnload">
+                      {`window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', 'G-ZX1S3FS4P4',{
+                        page_path: window.location.pathname,
+                      });`}
+                    </script>
                 </Head>
                 <body>
                     <Main />
