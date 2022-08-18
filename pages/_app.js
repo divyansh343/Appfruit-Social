7c4e53aadfae7598a300775d0d39f2f365408d85
divyansh343 <divyanshpal402@gmail.com>
2022-08-18 19:15:15 +0530

    contact page minimal update

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <title>theSocialCharts | Influencer marketing</title>
+        <title>The SocialCharts</title>
         <meta name='description' content="theSocialCharts is leading Influencer Marketing Agency in India connecting top brands with passionate socially engaged audiences through social media influencers.
         " />
       </Head>
